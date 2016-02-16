6123a85920c99dd1f2793523ad2f9148501ba551
Christian B <chriss@multi-byte.de>
2016-02-16 02:06:29 +0100

    removed ssh section from deploy since ssh got removed from dotfiles

--- deploy.sh
+++ deploy.sh
@@ -17,15 +17,3 @@ for link in `ls | grep -vE "README\.md|deploy\.sh"`; do
     ln -s $path/$link ~/.$link
 done
 [ -z "$verbose" ] && echo "Files deployed"
-
-
-# Set the correct access modes on SSH releated files
-[ -n "$verbose" ] && echo "Changing mod of ssh keys to 600 (private) and 644 (public)"
-chmod 600 $path/ssh/*
-chmod 644 $path/ssh/*\.pub
-
-for file in authorized_keys config known_hosts; do
-    [ -n "$verbose" ] && [ -e $path/ssh/config ] && echo "changing mod of $path/ssh/$file to 644"
-    [ -e $path/ssh/config ] && chmod 644 $path/ssh/$file
-done
-[ -z "$verbose" ] && echo "SSH file access checked"
