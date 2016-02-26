b5426091640e6853078821d17a187397eab247b9
Christian B <chriss@multi-byte.de>
2016-02-26 11:24:35 +0100

    fixed some logical errors

--- deploy.sh
+++ deploy.sh
@@ -6,29 +6,30 @@ main() {
   path=$( cd $(dirname $0) && pwd)
 
   # Update symlinks in home
-  for file in `ls | grep -vE "README\.md|deploy\.sh|config"`; do
-    link ${path}/${file} ~/.${file}
+  for file in `ls ${path} | grep -vE "README\.md|deploy\.sh|config"`; do
+    link ${path}/${file} ~/.${file} ${verbose}
   done
 
   mkdir -p ~/.config
-  for file in `ls config`; do
-    link ${path}/config/${file} ~/.config/${file}
+  for file in `ls ${path}/config`; do
+    link ${path}/config/${file} ~/.config/${file} ${verbose}
   done
 
   [ -z "$verbose" ] && echo "Files deployed"
 }
 
-# Usage: link target name
+# Usage: link target name [verbose]
 # creates Symlink 'name -> target'
 link() {
   target=$1
   name=$2
+  [ $# -gt 2 ] && verbose="true" || verbose=""
 
-  [ "x$verbose" == 'x' ] || echo "> ${name}"
+  [ "x${verbose}" == 'x' ] || echo "> ${name}"
 
   # remove symlink if exists
   if [ -h ${name} ]; then 
-    [ "x$verbose" == 'x' ] || echo "  removing ${name}"
+    [ "x${verbose}" == 'x' ] || echo "  removing ${name}"
     rm ${name}
   else
     if [ -e ${name} ]; then
@@ -37,12 +38,11 @@ link() {
     fi
   fi
 
-  [ "x$verbose" == 'x' ] || echo "  creating new symlink: ${name} -> ${target}"
+  [ "x${verbose}" == 'x' ] || echo "  creating new symlink: ${name} -> ${target}"
   ln -s ${target} ${name}
 }
 
 # run the programm
-main
-
+main $@
 
 # vim:set et ts=2 sw=2 sts=2:
